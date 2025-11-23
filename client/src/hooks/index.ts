76865f9c3ed526024a6a4c2eebd499018902f4ec
jamisays <jamisays@gmail.com>
2025-11-23 19:19:48 +0600

    feature: add sign in with Google on client

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,1 +1,5 @@
 export { useAuthNavigation } from "./useAuthNavigation";
+
+export { default as useRedirectIfAuthenticated } from "./useRedirectIfAuthenticated";
+
+export { default as useSocialAuth } from "./useSocialAuth";
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,7 @@
+import InitiateSocialAuth from "./initiateSocialAuth";
+
 export { default as extractErrorMessage} from "./extractErrorMessage";
 
 export { default as persistAuth} from "./PersistAuth";
+
+export const UseGoogle = () => InitiateSocialAuth("google-oauth2", "google");
