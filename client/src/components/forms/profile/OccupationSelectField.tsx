2b461873f4dc5deb5af7b15b4559cf1c4794f0dd
jamisays <jamisays@gmail.com>
2025-11-27 09:50:25 +0600

    feature: add edit profile form and page

---
+++ client/src/components/forms/profile/OccupationSelectField.tsx
@@ -0,0 +1,82 @@
+import { occupationOptions } from "@/constants";
+import { useGetUserProfileQuery } from "@/lib/redux/features/users/usersApiSlice";
+import { TProfileSchema } from "@/lib/validationSchemas";
+import { Occupation } from "@/types";
+import Select from "react-select";
+import { Briefcase } from "lucide-react";
+import dynamic from "next/dynamic";
+import React, { useEffect } from "react";
+import { Control, Controller, UseFormSetValue } from "react-hook-form";
+import customStyles from "../selectStyles";
+
+const ClientOnly = dynamic<{ children: React.ReactNode }>(
+	() => Promise.resolve(({ children }) => <>{children}</>),
+	{ ssr: false },
+);
+
+function isOccupation(value: any): value is Occupation {
+	return [
+		"mason",
+		"carpenter",
+		"plumber",
+		"roofer",
+		"painter",
+		"electrician",
+		"hvac",
+		"tenant",
+	].includes(value);
+}
+
+interface OccupationSelectFieldProps {
+	setValue: UseFormSetValue<TProfileSchema>;
+	control: Control<TProfileSchema>;
+}
+
+export default function OccupationSelectField({
+	setValue,
+	control,
+}: OccupationSelectFieldProps) {
+	const { data: profileData } = useGetUserProfileQuery();
+	const profile = profileData?.profile;
+
+	useEffect(() => {
+		if (profile?.gender) {
+			const occupationValue = occupationOptions.find(
+				(options) => options.value === profile.occupation,
+			);
+
+			if (occupationValue && isOccupation(occupationValue.value)) {
+				setValue("occupation", occupationValue.value);
+			}
+		}
+	}, [profile, setValue]);
+
+	return (
+    <div>
+			<label htmlFor="occupation" className="h4-semibold dark:text-babyPowder">
+				Occupation
+			</label>
+			<div className="mt-1 flex items-center space-x-3"></div>
+      <Briefcase className="dark:text-babyPowder size-8" />
+      <ClientOnly>
+        <Controller
+						control={control}
+						name="occupation"
+						render={({ field }) => (
+							<Select
+								className="mt-1 w-full"
+								{...field}
+								options={occupationOptions}
+								value={occupationOptions.find(
+									(option) => option.value === field.value,
+								)}
+                onChange={(option) => field.onChange(option?.value)}
+                instanceId="occupation-select"
+                styles={customStyles}
+							/>
+						)}
+					/>
+      </ClientOnly>
+    </div>
+  );
+}
