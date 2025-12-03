0192e98e75f86d8cfbf305f6513a70d87e6d6323
jamisays <jamisays@gmail.com>
2025-12-03 07:19:18 +0600

    feature: add tenants card with basic info

--- client/src/app/(estate)/tenants/page.tsx
+++ client/src/app/(estate)/tenants/page.tsx
@@ -1,35 +1,17 @@
-"use client";
+
+import TenantCard from '@/components/cards/TenantCard';
 import ProtectedRoute from '@/components/shared/ProtectedRoutes';
-import Spinner from '@/components/shared/Spinner';
-import { useGetAllUsersQuery } from '@/lib/redux/features/users/usersApiSlice';
-import React from 'react'
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Alpha Apartments | Tenants",
+  description: "Authenticated users can view basic information about other tanants within the property. Tenants can also search for other tenants"
+}
 
 function TenantsPageContent() {
-  // 1. Use the auto-generated hook
-  const {data, isLoading} = useGetAllUsersQuery({})
-
-  // 2. Handle Loading State
-  if(isLoading) {
-
-    // 3. Render the Data
-    return (
-      <div className='flex-center pt-2'>
-        <Spinner size="xl"/>
-      </div>
-    )
-  }
   return (
     <div>
-      <h1 className='dark:text-pumpkin text-6xl'>Tenants</h1>
-      {/* Check if data exists */}
-      {data && data.profiles.results.length > 0 ? (
-        // Map through results and verify tenant details
-        data.profiles.results.map((tenant) => (
-          <p key={tenant.id} className='text-2xl dark:text-lime-500'>{tenant.full_name} - {tenant.occupation}</p>
-        ))
-      ) : (
-        <p className='text-2xl dark:text-lime-500'>No tenants found.</p>
-      )}
+      <TenantCard /> 
     </div>
   )
 }
