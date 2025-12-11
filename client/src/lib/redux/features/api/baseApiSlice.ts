b41b3928061ff058260ea405b7b37e302f71b8db
jamisays <jamisays@gmail.com>
2025-12-11 06:35:48 +0600

    feature: add postApiSlice & validation schemas

--- client/src/lib/redux/features/api/baseApiSlice.ts
+++ client/src/lib/redux/features/api/baseApiSlice.ts
@@ -65,7 +65,7 @@ const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQue
 export const baseApiSlice = createApi({
   reducerPath: "api",
   baseQuery: baseQueryWithReauth,
-  tagTypes: ["User", "Apartment", "Issue", "Report"], 
+  tagTypes: ["User", "Apartment", "Issue", "Report", "Post"], 
   refetchOnFocus: true,
   refetchOnMountOrArgChange: true,
   endpoints: (builder) => ({}),
